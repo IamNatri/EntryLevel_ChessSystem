23869e52d84e7954788f73b08dce6812ca6ecdf9
Matheus bastos <105763337+IamNatri@users.noreply.github.com>
2023-09-20 11:11:17 -0300

    BoardException and defensive programming

---
+++ src/IamNatri/com/github/entrylevel_chessSysten/boardgame/BoardException.java
@@ -0,0 +1,11 @@
+package IamNatri.com.github.entrylevel_chessSysten.boardgame;
+
+import java.io.Serial;
+
+public class BoardException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public BoardException(String msg) {
+        super(msg);
+    }
+}
